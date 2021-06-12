4a0f5d945279138f215e4a56f131bcedab5fbdde
Andrés K. Aquin <andruuus123@gmail.com>
2021-06-12 17:42:39 -0400

    first commit

---
+++ 2.js
@@ -0,0 +1,33 @@
+var numero = 5;
+console.log("Numero: ",numero)
+
+//Variables de cadena o texto
+
+var cadena = "Hola Mundo";
+console.log("Saludo: ",cadena);
+
+//Variables de tipo booleana
+
+var boleana = true;
+console.log("Boolean:",boleana);
+
+//Variables de tipo Arreglo
+
+var semana = ["Lunes","Martes","Miercoles","Jueves"];
+console.log("Semana:", semana);
+
+//Variables de tipo Objeto
+
+var persona = {edad:"22",
+              color:"moreno",
+             genero:"Mujer",
+          est_civil:"soltero",
+          altura:"1.5"
+}
+console.log("Atributos",persona);
+console.log("Genero: ",persona.genero);
+
+// DOM (Document Object Model)
+
+var caja = document.querySelector('#caja');
+console.log("Caja: ",caja);
